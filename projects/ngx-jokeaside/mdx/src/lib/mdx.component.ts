5fb97061ead891aa1155dcb4d74f46ea825b21b8
Salathiel <salathiel@genese.name>
2025-05-01 22:22:27 +0200

    feat: add multile selectors for component
    Signed-off-by: Salathiel <salathiel@genese.name>

--- projects/ngx-jokeaside/mdx/src/lib/mdx.component.ts
+++ projects/ngx-jokeaside/mdx/src/lib/mdx.component.ts
@@ -2,7 +2,7 @@ import { MdxService } from './mdx.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '.ngx-jokeaside-mdx',
+  selector: 'ngx-jokeaside-mdx, [ngx-jokeaside-mdx], .ngx-jokeaside-mdx',
   template: `
     <p>
       {{ mdxService.sayItWorks("mdx") }}
